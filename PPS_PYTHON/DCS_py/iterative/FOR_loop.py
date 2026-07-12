e4039d87f930fe606714cad95b3fc341e4595426
Charan16-bit <charancj34@gmail.com>
2026-07-12 09:23:38 +0530

    Auto update 12-07-2026  9:23:38.41

--- PPS_PYTHON/DCS_py/iterative/FOR_loop.py
+++ PPS_PYTHON/DCS_py/iterative/FOR_loop.py
@@ -48,7 +48,53 @@ for i in range(i,n +1 ):
      if(n%i==0 ):
       count = count + i
 if(count == n):
-         print("Secure ID")
+         print("Perfect ID")
 else:
-        print("Insecure ID")
+        print("imperfect ID")
 
+#🏆 Algorithm 3 – GCD using for
+a = 24 
+b = 36
+gcd = 1
+for i in range(1,min(a,b)+1):
+    if(a % i ==0) and (b % i ==0):
+        gcd = i
+print(gcd)
+#🏆 Algorithm 4 – LCM using for
+a = 12
+b = 18
+lcm = 0 
+for i in range (max(a,b),a*b+1):
+    if(i % a == 0) and (i % b ==0):
+        lcm = i
+        break
+print(lcm)
+
+#🤖 Robot Energy Generation
+n = 10
+first = 0
+second = 1 
+for i in range(first , n ):
+
+    print(first,end = " ") 
+    sum = first + second
+    first = second
+    second = sum 
+print("/a")
+#🏆 Algorithm 6 – Reverse Number using for
+n = 947531
+reverse = 0 
+digit = len(str(n))
+for i in range(digit):
+    digit = n % 10
+    reverse = reverse * 10 + digit 
+    n = n // 10
+print(reverse)
+
+#🏆 Algorithm 7 – Multiplication Table (for)
+n = 10 
+mutliply = 1
+for i in range(1,n+1):
+    if(n * i == 7):
+     mutliply = mutliply * 7
+     print("7","x",i,"=",mutliply)
