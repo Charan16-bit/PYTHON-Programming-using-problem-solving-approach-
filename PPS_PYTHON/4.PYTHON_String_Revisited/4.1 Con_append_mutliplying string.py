f0a6b7cc699519aec356387ddfbee87497980a36
Charan16-bit <charancj34@gmail.com>
2026-08-31 19:13:18 +0530

    Auto update 31-08-2026 19:13:18.79

---
+++ PPS_PYTHON/4.PYTHON_String_Revisited/4.1 Con_append_mutliplying string.py
@@ -0,0 +1,29 @@
+##concation
+fname = "charan"
+llast = "raj"
+
+print(fname + llast)
+
+###Mutliplying 
+
+s = "***"
+print(s*2)
+
+#### append
+
+s = "Raj"
+
+s1 = " RA231100312384"
+
+s = "charan" + s + s1
+print(s)
+### string immutable 
+s = "python"
+
+print(s[0])
+###
+
+s2 = "PYTHON"
+s2 = "j" + s[1:]
+
+print(s2)
