3dd0f357eea1d18a12ebaed0d5766e7c00c6282f
Charan16-bit <charancj34@gmail.com>
2026-09-02 11:40:38 +0530

    Auto update 02-09-2026 11:40:38.56

--- PPS_PYTHON/4.PYTHON_String_Revisited/4.1 Con_append_mutliplying string.py
+++ PPS_PYTHON/4.PYTHON_String_Revisited/4.1 Con_append_mutliplying string.py
@@ -27,3 +27,70 @@ s2 = "PYTHON"
 s2 = "j" + s[1:]
 
 print(s2)
+
+###
+
+course = "python"
+
+course = course[:3] + "j" + course[4:]
+
+print(course)
+
+#🔥 ROUND 3 — PREDICT THE OUTPUT
+#1
+s = "banana"
+print(s.count("an"))
+
+#2
+
+s = "banana"
+s = s[:2] + "x" + s[3:]
+
+#3
+
+text = "Python Is Easy"
+
+print(text.lower().replace("easy","fun"))
+
+#4
+data = "A,B,C"
+x = data.split(",")
+print(x)
+print(len(x))
+
+#5
+letters = ["A","B","C","D","E","G"]
+print(" ".join(letters))
+
+#6
+s = "123"
+print(s.isdigit(),s.isalnum())
+
+###💻 ROUND 4 — CODING CHALLENGE#
+
+name = "charan raj"
+print(name.upper().replace(" ","_"))
+
+##2
+
+course_feedback = "python is very easy"
+
+cout = course_feedback.split()
+print(cout)
+print(len(cout))
+
+#Q23
+email = " charan@gmail.com"
+
+if email.find("@") != -1:
+    print("valid")
+
+else:
+    print("invalid")
+
+
+#q24
+
+h = "   hello Python  "
+
+print(h.strip())
